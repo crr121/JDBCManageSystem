1cb7921e09553a19942383c0beed92e499b1bdd3
Your Name <you@example.com>
2017-08-13 23:25:37 +0800

    jdbcutil

--- src/com/zt/util/JDBCUtil.java
+++ src/com/zt/util/JDBCUtil.java
@@ -1,5 +1,11 @@
 package com.zt.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 
 public class JDBCUtil {
@@ -35,5 +41,64 @@ public class JDBCUtil {
 	}
 	
 	//获取连接
+	//获取连接采用驱动管理来获取连接
+	//同样的获取连接我们也定义一个函数来执行，不过这里不再是一个静态块
+	//而是要给公共函数，返回一个connection的类型参数
+	public Connection getConnection(){
+		//为了先不报错，先定义一个connection的变量con用于返回
+		Connection con = null;
+		//这里的url user password 都需要通过资源包来获取相应的字段名的内容
+		try {
+			//注意这里的url user pwd 都是资源包jdbc.properties里面的键值对的键的字段名，是字符串格式需要引号
+			//而不是变量，相当于这里是一个map,通过键（这里的键是字符串的形式）来获取对应的值
+			DriverManager.getConnection(rb.getString("url"), rb.getString("user"), rb.getString("pwd"));
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return con;
+	}
+	
 	//关闭连接
+	//关闭连接的顺序是结果集rs   预处理ps/st   连接 con
+	//由于他们都有初值null
+	//再关闭之前都需要先判断是否为空，为空的话就不用关闭操作了
+	//这里的关闭没有返回值
+	//但是需要传入三个需要关闭的参数进来
+	public void closeALL(ResultSet rs,PreparedStatement ps,Connection con){
+		
+			try {
+				if(rs!=null){
+					rs.close();
+				}
+				if(ps!=null){
+					ps.close();
+				}
+				if(con!=null){
+					con.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+	} 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
